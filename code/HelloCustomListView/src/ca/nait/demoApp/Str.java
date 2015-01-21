b579e67b9db97215d3891602406758ed6126ae79
Dan Gilleland <dgilleland@nait.ca>
2015-01-21 16:14:32 -0700

    Add comments & refactor code

--- code/HelloCustomListView/src/ca/nait/demoApp/Str.java
+++ code/HelloCustomListView/src/ca/nait/demoApp/Str.java
@@ -2,7 +2,7 @@ package ca.nait.demoApp;
 
 public class Str {
     public static boolean isEmptyString(String str) {
-        return !(str != null && str.trim().length() > 0);
+        return str == null || str.trim().length() == 0;
     }
 
     public static boolean isNotEmptyString(String str) {
