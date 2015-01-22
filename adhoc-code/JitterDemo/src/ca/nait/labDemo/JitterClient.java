434f13fb6c9c70bd30841690c0b3d849eac474cd
Dan Gilleland <dgilleland@nait.ca>
2015-01-21 17:05:50 -0700

    Update JitterDemo to use a ListView (incomplete)

--- adhoc-code/JitterDemo/src/ca/nait/labDemo/JitterClient.java
+++ adhoc-code/JitterDemo/src/ca/nait/labDemo/JitterClient.java
@@ -7,6 +7,8 @@ import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
@@ -24,6 +26,49 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class JitterClient {
+	public List<HashMap<String, String>> getJitterDetails() 
+			throws IllegalStateException, URISyntaxException, IOException {
+		List<HashMap<String, String>> jitters = new ArrayList<HashMap<String, String>>();
+		// .....
+		HttpResponse response = callJitterWebService("http://www.youcode.ca/JitterServlet");
+		
+		BufferedReader in = new BufferedReader(
+							new InputStreamReader(response.getEntity().getContent()));
+		
+		String line = "";
+		
+		while((line = in.readLine()) != null) {
+			// Name of person
+			String name = line;
+			// Post
+			String post = in.readLine();
+			// Date
+			String date = in.readLine();
+			
+			// Make a hashmap and put the items in it
+			HashMap<String, String> data = new HashMap<String, String>();
+			data.put("personName", name);
+			data.put("postedText", post);
+			data.put("postedOnDate", date);
+			
+			// Add it to my list
+			jitters.add(data);
+		}
+		in.close();
+		return jitters;
+	}
+	
+	private HttpResponse callJitterWebService(String url) 
+            throws URISyntaxException, 
+     	   IllegalStateException,
+            IOException {
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet();
+		request.setURI(new URI(url));
+		HttpResponse response = client.execute(request);
+		return response;
+	}
+	
 	public List<String> getJitters()  
             throws URISyntaxException, 
             	   IllegalStateException,
@@ -31,10 +76,11 @@ public class JitterClient {
 		BufferedReader in = null;
 		List<String> jitters = new ArrayList<String>();
 		
-		HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet();
-		request.setURI(new URI("http://www.youcode.ca/JSONServlet"));
-		HttpResponse response = client.execute(request);
+		HttpResponse response = callJitterWebService("http://www.youcode.ca/JSONServlet");
+//		HttpClient client = new DefaultHttpClient();
+//		HttpGet request = new HttpGet();
+//		request.setURI(new URI("http://www.youcode.ca/JSONServlet"));
+//		HttpResponse response = client.execute(request);
 		
 		in = new BufferedReader(
 				new InputStreamReader(response.getEntity().getContent()));
