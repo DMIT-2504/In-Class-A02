8a2bff5098c65c1cec994ef70a7f8d6077c4eee5
jstevenson7 <jstevenson7@WA320-04.nait.ca>
2015-01-26 13:00:57 -0700

    b

---
+++ ReviewApp/src/ca/reviewApp/ReviewAppActivity.java
@@ -0,0 +1,13 @@
+package ca.reviewApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReviewAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
